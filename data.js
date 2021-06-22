37ab5b6731880dc1c044a7e587aee28122534e41
AJ-Brown-InTech <75697769+AJ-Brown-InTech@users.noreply.github.com>
2021-06-22 11:06:43 -0700

    finished

--- data.js
+++ data.js
@@ -5,36 +5,52 @@ async function getBooks() {
 getBooks()
 
 
-
 // Getting books from api & creating a new array to build html
 async function showBooks() {
   let books = await getBooks()
   let bookGridHTMLArray = books.map(book => getHtmlForBook(book))
-  console.log(bookGridHTMLArray)
-  let booksGridHtml = bookGridHTMLArray.join("")
-  console.log(booksGridHtml) // not sure if i need to add parameters for this function
-
+  let booksGridHtml = bookGridHTMLArray.join("") 
+// iterate through the books 
   document.getElementById('main-content').innerHTML = booksGridHtml
 }
 
 showBooks()
+
 /* Building a html document too build the book cards.
 the book image, Book rank - Book Title, Book description, Clicking on the book image or title should redirect to the Amazon page for the book. */
 
 function getHtmlForBook(book) {
-
-  let bookSummaryHtml = 
-  `
-  {<div class="card">
-  <a href ="${book.amazon_product_url}" target ="_blank"
+  let bookSummaryHtml = `
+  <div class="card">
+  <a href ="${book.amazon_product_url}" class="one" target="_blank">
              <img src="${book.book_image}" class="card-img-top">
              <div class="card-body">
-                    <h5 class="card-title">Title:${book.title }</h5>
-                    <p class="card-text">Rank: ${book.rank} <br> Description: ${book.description}</p>
+
+                    <h5 class="card-title">Title: ${book.title}</h5>
+                    </a>
+                    <p class="card-text"> <strong>Rank:</strong> ${book.rank} <br> <strong> Description</strong>: ${book.description}</p>
               </div>
-              </a>
-      </div> */}
- ` 
+      </div> 
+ `
   return bookSummaryHtml
 }
-getHtmlForBook(book)
+
+
+//adding event listener to pull specific book
+async function clickMyBooks() {
+  let books = await getBooks()
+  books.forEach(x=> {
+    let button = document.getElementById(`${x.rank}-link`)
+    button.addEventListener("click", (event) => {
+      event.preventDefault()
+      showBook(x)
+    })
+  })
+}
+
+clickMyBooks()
+
+
+function showBook(book){
+  document.getElementById('main-content').innerHTML = getHtmlForBook(book)
+}
