c23bace9c91742b4ea016c586dac7b120df17991
AJ-Brown-InTech <75697769+AJ-Brown-InTech@users.noreply.github.com>
2021-06-21 17:36:27 -0700

    html build in js

--- data.js
+++ data.js
@@ -2,9 +2,10 @@ async function getBooks() {
   const response = await axios.get(`https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=5Vd8O8baGS3WEG1eQVAaS2mG6K0VyHH8`)
   return response.data.results.books;
 };
+getBooks()
 
 
-// console.log()
+
 // Getting books from api & creating a new array to build html
 async function showBooks() {
   let books = await getBooks()
@@ -14,38 +15,26 @@ async function showBooks() {
   console.log(booksGridHtml) // not sure if i need to add parameters for this function
 
   document.getElementById('main-content').innerHTML = booksGridHtml
-
 }
+
 showBooks()
 /* Building a html document too build the book cards.
 the book image, Book rank - Book Title, Book description, Clicking on the book image or title should redirect to the Amazon page for the book. */
 
 function getHtmlForBook(book) {
 
-  let bookSummaryHtml = `
- <article>
-    <a href ="${
-    book.amazon_product_url
-  }" target ="_blank" 
-        <img src ="${
-    book.book_image
-  }" alt="nytimesimg"/>
-            <span> ${
-    book.title
-  } - Rank Number:${
-    book.rank
-  }
-            </span>
-        </a>
-    <p>${
-    book.description
-  }</p>
-</article>
- `
-  return bookSummaryHtml}{ /* <div class="card">
-              <img src="${animal.imageURL}" class="card-img-top">
-              <div class="card-body">
-                     <h5 class="card-title">${animal.name }</h5>
-                     <p class="card-text">sex: ${animal.sex} age: ${animal.age }</p>
-               </div>
-       </div> */}
+  let bookSummaryHtml = 
+  `
+  {<div class="card">
+  <a href ="${book.amazon_product_url}" target ="_blank"
+             <img src="${book.book_image}" class="card-img-top">
+             <div class="card-body">
+                    <h5 class="card-title">Title:${book.title }</h5>
+                    <p class="card-text">Rank: ${book.rank} <br> Description: ${book.description}</p>
+              </div>
+              </a>
+      </div> */}
+ ` 
+  return bookSummaryHtml
+}
+getHtmlForBook(book)
